baceb9bdf51b5aa17ba5de636628d6e8ed86347c
Rhenn Handel <rwhandel@gmail.com>
2024-02-11 00:26:07 -0500

    fullscreen and volume

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -4,10 +4,20 @@ extends Node2D
 
 func _ready() -> void:
 	MusicManager.play(title_music)
-
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"),linear_to_db($Control/VBoxContainer2/HSlider.value))
 
 func _on_begin_pressed():
 	SceneManager.switch_scene("main_level")
 
 func _on_end_pressed():
 	SceneManager.quit_game()
+
+
+func _on_h_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"),linear_to_db(value))
+
+func _on_check_box_toggled(toggled_on):
+	if toggled_on:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
